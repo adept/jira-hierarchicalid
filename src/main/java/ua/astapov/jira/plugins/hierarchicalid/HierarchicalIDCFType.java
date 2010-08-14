0a9a7f7501e492c253589ff7f643cf3902146b99
Dmitry Astapov <dastapov@gmail.com>
2010-08-14 23:39:18 +0300

    Implement methods necessary to remove options.
    Remove obsolete HierarchicalIDOptions

--- src/main/java/ua/astapov/jira/plugins/hierarchicalid/HierarchicalIDCFType.java
+++ src/main/java/ua/astapov/jira/plugins/hierarchicalid/HierarchicalIDCFType.java
@@ -5,7 +5,8 @@
 package ua.astapov.jira.plugins.hierarchicalid;
 
 import java.util.List;
-import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
 
 import org.apache.log4j.Logger;
 
@@ -13,18 +14,20 @@ import com.atlassian.jira.ComponentManager;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.context.JiraContextNode;
 import com.atlassian.jira.issue.customfields.MultipleCustomFieldType;
+import com.atlassian.jira.issue.customfields.MultipleSettableCustomFieldType;
 import com.atlassian.jira.issue.customfields.SortableCustomField;
-import com.atlassian.jira.issue.customfields.manager.OptionsManager;
-import com.atlassian.jira.issue.customfields.option.Options;
+import com.atlassian.jira.issue.customfields.config.item.SettableOptionsConfigItem;
 import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.customfields.manager.OptionsManager;
+import com.atlassian.jira.issue.customfields.option.Option;
+import com.atlassian.jira.issue.customfields.option.Options;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.config.FieldConfig;
 import com.atlassian.jira.issue.link.IssueLink;
-import com.atlassian.jira.issue.customfields.config.item.SettableOptionsConfigItem;
-import ua.astapov.jira.plugins.hierarchicalid.config.HierarchicalIDOption;
+import com.atlassian.jira.util.EasyList;
 @SuppressWarnings({ "unchecked", "rawtypes" })
-public class HierarchicalIDCFType extends CalculatedCFType implements SortableCustomField, MultipleCustomFieldType 
+public class HierarchicalIDCFType extends CalculatedCFType implements SortableCustomField, MultipleCustomFieldType, MultipleSettableCustomFieldType 
 {
     private static final Logger log = Logger.getLogger(HierarchicalIDCFType.class);
 
@@ -54,19 +57,10 @@ public class HierarchicalIDCFType extends CalculatedCFType implements SortableCu
 
     public Object getValueFromIssue(CustomField field, Issue issue)
     {
-    	// get the relevant configuration for the issue
-    	FieldConfig fieldConfig = field.getRelevantConfig(issue);
-    	log.error("fieldConfig: "+fieldConfig.toString());
-
     	// Get a list of stored options for the field
-    	Options options = optionsManager.getOptions(fieldConfig);
-    	log.error("options: "+options.toString());
+    	Options options = optionsManager.getOptions(field.getRelevantConfig(issue));
 
-    	// build the custom field option object
-    	HierarchicalIDOption customFieldOptions = new HierarchicalIDOption(options);
-    	log.error("customFieldOptions: "+customFieldOptions.toString());
-
-    	String linkTypeName = customFieldOptions.getLinkTypeName();
+    	String linkTypeName = ((Option) options.getRootOptions().get(0)).getValue();
         log.debug("linkTypeName: " + linkTypeName);
 
         // incidents linked to this one
@@ -100,14 +94,20 @@ public class HierarchicalIDCFType extends CalculatedCFType implements SortableCu
     
     public List getConfigurationItemTypes()
     {
-    	List configurationItemTypes = new LinkedList(); 
-    	configurationItemTypes.add(new SettableOptionsConfigItem(this, 
-    			optionsManager));
-    	return configurationItemTypes;
+    	return EasyList.build(new SettableOptionsConfigItem(this, optionsManager));
     }
     
     public Options getOptions(FieldConfig config, JiraContextNode jiraContextNode)
     {
     	return this.optionsManager.getOptions(config);
     }
+
+	public Set getIssueIdsWithValue(CustomField arg0, Option arg1) {
+		// TODO Auto-generated method stub
+		return new HashSet();
+	}
+
+	public void removeValue(CustomField arg0, Issue arg1, Option arg2) {
+		// TODO Auto-generated method stub
+	}
 }
